b7535401d4e288ee8d51abbd3341ebf48c68a4fe
elisaflemer <elisaflemer@gmail.com>
2024-02-15 23:32:25 -0300

    test(mqtt): :white_check_mark: finishing tets

--- src/tests/test_publisher.py
+++ src/tests/test_publisher.py
@@ -26,13 +26,15 @@ def test_connect_mqtt():
 
 def setup_test():
     def on_message(client, userdata, message):
-        print("Received message: ", message.payload.decode())
-        global received_messages, first_message_timestamp, last_message_timestamp
+
+        global received_messages
+        global first_message_timestamp
+        global last_message_timestamp
+
         received_messages.append(message.payload.decode())
         if len(received_messages) == 1:
             first_message_timestamp = time.time()
         last_message_timestamp = time.time()
-
 
     global received_messages
     received_messages = []
@@ -62,7 +64,6 @@ def test_message_reception():
 
 def test_message_integrity():
     setup_test()  # Assuming setup_test() is defined elsewhere
-    
     decoded_messages = []
     for msg in received_messages:
         m = json.loads(msg)
@@ -71,10 +72,13 @@ def test_message_integrity():
     for expected_value in mock_data:
         assert any(msg['value'] == expected_value for msg in decoded_messages)
 
+
 def test_transmission_rate():
     setup_test()  # Assuming setup_test() is defined elsewhere
     print(first_message_timestamp, last_message_timestamp)
-    time_period = timedelta(seconds=(last_message_timestamp - first_message_timestamp)).total_seconds()
+    time_period = timedelta(
+        seconds=(last_message_timestamp - first_message_timestamp)
+        ).total_seconds()
     frequency = len(mock_data) / time_period
 
     # Check transmission rate
@@ -93,29 +97,48 @@ def test_qos():
 
     client.on_message = on_message
 
-    client.subscribe(f'sensor/{mock_config.region}/{mock_config.sensor}', mock_config.qos)
-    
+    client.subscribe(f'sensor/{mock_config.region}/{mock_config.sensor}',
+                     mock_config.qos)
+
     mock_qos_data = [1.25]
     publish_data(client, mock_config, mock_qos_data)
 
     if mock_config.qos == 0:
-        print("\x1b[33m[INFO] QoS set to 0, no guarantee of message delivery\x1b[0m")
+        print(
+            "\x1b[33m[INFO] QoS set to 0, no guarantee of message delivery"
+            "\x1b[0m"
+        )
     elif mock_config.qos == 1:
         if len(received_messages) == 0:
-            raise AssertionError("\x1b[31m[FAIL] No messages received with QoS 1\x1b[0m")
+            raise AssertionError(
+                "\x1b[31m[FAIL] No messages received with QoS 1\x1b[0m"
+                )
         else:
             for msg in received_messages:
                 m = json.loads(msg)
                 if m['value'] != mock_qos_data[0]:
-                    raise AssertionError("\x1b[31m[FAIL] Received {}, expected {}\x1b[0m".format(m['value'], mock_qos_data[0]))
+                    raise AssertionError(
+                        "\x1b[31m[FAIL] Received {}, expected {}\x1b[0m"
+                        .format(m['value'], mock_qos_data[0])
+                        )
             print("\x1b[32m[PASS] Message received with QoS 1\x1b[0m")
     elif mock_config.qos == 2:
         if len(received_messages) != 1:
-            raise AssertionError("\x1b[31m[FAIL] Incorrect number of messages received with QoS 2. Expected: 1, received: {}\x1b[0m".format(len(received_messages)))
+            raise AssertionError(
+                "\x1b[31m[FAIL] Incorrect number of messages received"
+                "with QoS 2. Expected: 1, received: {}\x1b[0m"
+                .format(len(received_messages)))
         else:
             m = json.loads(received_messages[0])
             if m['value'] != mock_qos_data[0]:
-                raise AssertionError("\x1b[31m[FAIL] Received {}, expected {}\x1b[0m".format(m['value'], mock_qos_data[0]))
+                raise AssertionError(
+                    "\x1b[31m[FAIL] Received {}, expected {}\x1b[0m"
+                    .format(m['value'], mock_qos_data[0])
+                )
             print("\x1b[32m[PASS] Message received with QoS 2\x1b[0m")
     else:
-        raise AssertionError("\x1b[31m[FAIL] Invalid QoS value: {}\x1b[0m".format(mock_config['qos']))
+        raise AssertionError(
+            "\x1b[31m[FAIL] Invalid QoS value: {}\x1b[0m".format(
+                mock_config.qos
+                )
+            )
