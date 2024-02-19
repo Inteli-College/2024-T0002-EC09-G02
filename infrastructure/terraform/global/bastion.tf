0e9c7e7f0a8ca32dfcc6e1ecfdcb4371596698d8
Gustavo Ferreira <gustavo.oliveira@sou.inteli.edu.br>
2024-02-18 21:06:06 -0300

    fix: update infra

--- infrastructure/terraform/global/bastion.tf
+++ infrastructure/terraform/global/bastion.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "bastion_host" {
-  ami           = "ami-053b0d53c279acc90"
-  instance_type = "t2.micro"
-  key_name      = "tutorial-ec2"
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t3.micro"
+  key_name      = "ec2_key_pair"
 
   subnet_id                   = aws_subnet.public_subnet_az1.id
   associate_public_ip_address = true
