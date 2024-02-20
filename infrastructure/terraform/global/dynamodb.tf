4bcf8b156ef03be722c3e2c5e46a322f1b1ece68
Gustavo Ferreira <gustavo.oliveira@sou.inteli.edu.br>
2024-02-20 09:49:33 -0300

    feat: pipe creating dynamo tables

---
+++ infrastructure/terraform/global/dynamodb.tf
@@ -0,0 +1,29 @@
+resource "aws_dynamodb_table" "sensor_north_particle" {
+  name           = "sensor_north_particle"
+  billing_mode   = "PROVISIONED"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Region North Table"
+  }
+}
+
+resource "aws_dynamodb_table" "sensor_west_particle" {
+  name           = "sensor_west_particle"
+  billing_mode   = "PROVISIONED"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Region West Table"
+  }
+}
