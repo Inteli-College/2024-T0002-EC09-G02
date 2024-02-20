a67619f99cc0e4bbc77b0f5825a6b09f0b7aaaa1
Gustavo Ferreira <gustavo.oliveira@sou.inteli.edu.br>
2024-02-20 10:06:53 -0300

    fix: s3 create files

--- infrastructure/terraform/global/s3.tf
+++ infrastructure/terraform/global/s3.tf
@@ -21,3 +21,29 @@ resource "aws_s3_bucket_object" "infrastructure_state_terraform_folder" {
   acl    = "public-read"
   content_type = "application/x-directory"
 }
+
+resource "aws_cloudfront_origin_access_identity" "s3_origin_access_identity" {
+  comment = "S3 origin access identity for store-bucket"
+}
+
+resource "aws_s3_bucket_policy" "public_access" {
+  bucket = aws_s3_bucket.infrastructure_state_terraform.id
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Id": "Policy1692122833651",
+    "Statement": [
+        {
+            "Sid": "Stmt1692122770670",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:DeleteObject"
+            ],
+            "Resource": "${aws_s3_bucket.infrastructure_state_terraform.arn}/state-files/*"
+        }
+    ]
+  })
+}
