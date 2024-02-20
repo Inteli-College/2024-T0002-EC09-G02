7831e3241979652f9fa7c5f93cc5b172e575c387
Gustavo Ferreira <gustavo.oliveira@sou.inteli.edu.br>
2024-02-20 10:20:38 -0300

    fix: s3 create files

--- infrastructure/terraform/global/s3.tf
+++ infrastructure/terraform/global/s3.tf
@@ -1,12 +1,9 @@
 resource "aws_s3_bucket" "infrastructure_state_terraform" {
   bucket_prefix = "infrastructure-state-terraform"
-  versioning {
-    enabled = true
-  }
 
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["GET"]
+    allowed_methods = ["GET, PUT, POST, DELETE, HEAD"]
     allowed_origins = ["*"]
     expose_headers  = []
     max_age_seconds = 3000
@@ -21,28 +18,6 @@ resource "aws_s3_bucket_object" "infrastructure_state_terraform_folder" {
 
 resource "aws_cloudfront_origin_access_identity" "s3_origin_access_identity" {
   comment = "S3 origin access identity for store-bucket"
-}
-
-resource "aws_s3_bucket_policy" "public_access" {
-  bucket = aws_s3_bucket.infrastructure_state_terraform.id
-
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Id": "Policy1692122833651",
-    "Statement": [
-        {
-            "Sid": "Stmt1692122770670",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject",
-                "s3:PutObject",
-                "s3:DeleteObject"
-            ],
-            "Resource": "${aws_s3_bucket.infrastructure_state_terraform.arn}/state-files/*"
-        }
-    ]
-  })
 }
 
 resource "aws_s3_bucket_ownership_controls" "infrastructure_state_terraform_oc" {
