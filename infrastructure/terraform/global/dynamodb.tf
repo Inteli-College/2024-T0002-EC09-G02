5918094ba519ccfa6a91128fcfec09d01a56fec0
Gustavo Ferreira <gustavo.oliveira@sou.inteli.edu.br>
2024-02-24 18:16:44 -0300

    feat: corrigindo as rules

--- infrastructure/terraform/global/dynamodb.tf
+++ infrastructure/terraform/global/dynamodb.tf
@@ -1,5 +1,5 @@
-resource "aws_dynamodb_table" "sensor_north_particle" {
-  name         = "sensor_north_particle"
+resource "aws_dynamodb_table" "sensor_north" {
+  name         = "sensor_north"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
 
@@ -13,8 +13,38 @@ resource "aws_dynamodb_table" "sensor_north_particle" {
   }
 }
 
-resource "aws_dynamodb_table" "sensor_west_particle" {
-  name         = "sensor_west_particle"
+resource "aws_dynamodb_table" "sensor_south" {
+  name         = "sensor_south"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Region South Table"
+  }
+}
+
+resource "aws_dynamodb_table" "sensor_east" {
+  name         = "sensor_east"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Region East Table"
+  }
+}
+
+resource "aws_dynamodb_table" "sensor_west" {
+  name         = "sensor_west"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
 
@@ -25,5 +55,126 @@ resource "aws_dynamodb_table" "sensor_west_particle" {
 
   tags = {
     Name = "Region West Table"
+  }
+}
+
+resource "aws_dynamodb_table" "sensor_center" {
+  name         = "sensor_center"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Region Center Table"
+  }
+}
+
+# Policy to allow IoT to write to DynamoDB
+resource "aws_iam_policy" "iot_dynamodb_policy" {
+  name   = "iot_dynamodb_policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "dynamodb:PutItem"
+      ],
+      "Resource": [
+        "${aws_dynamodb_table.sensor_north.arn}",
+        "${aws_dynamodb_table.sensor_south.arn}",
+        "${aws_dynamodb_table.sensor_east.arn}",
+        "${aws_dynamodb_table.sensor_west.arn}",
+        "${aws_dynamodb_table.sensor_center.arn}"]
+    }
+  ]
+}
+EOF
+}
+
+
+# Rule to send data from IoT Core to DynamoDB
+resource "aws_iot_topic_rule" "sensor_north_topic_rule" {
+  name        = "SensorNorthRule"
+  description = "Sensor North rule to send data to DynamoDB"
+  enabled     = true
+
+  sql         = "SELECT * FROM 'sensor_north'"
+  sql_version = "2016-03-23"
+
+  dynamodbv2 {
+    role_arn = var.lab_role
+    put_item {
+      table_name = aws_dynamodb_table.sensor_north.name
+    }
+  }
+}
+
+resource "aws_iot_topic_rule" "sensor_south_topic_rule" {
+  name        = "SensorSouthRule"
+  description = "Sensor South rule to send data to DynamoDB"
+  enabled     = true
+
+  sql         = "SELECT * FROM 'sensor_south'"
+  sql_version = "2016-03-23"
+
+  dynamodbv2 {
+    role_arn = var.lab_role
+    put_item {
+      table_name = aws_dynamodb_table.sensor_south.name
+    }
+  }
+}
+
+resource "aws_iot_topic_rule" "sensor_east_topic_rule" {
+  name        = "SensorEastRule"
+  description = "Sensor East rule to send data to DynamoDB"
+  enabled     = true
+
+  sql         = "SELECT * FROM 'sensor_east'"
+  sql_version = "2016-03-23"
+
+  dynamodbv2 {
+    role_arn = var.lab_role
+    put_item {
+      table_name = aws_dynamodb_table.sensor_east.name
+    }
+  }
+}
+
+resource "aws_iot_topic_rule" "sensor_west_topic_rule" {
+  name        = "SensorWestRule"
+  description = "Sensor West rule to send data to DynamoDB"
+  enabled     = true
+
+  sql         = "SELECT * FROM 'sensor_west'"
+  sql_version = "2016-03-23"
+
+  dynamodbv2 {
+    role_arn = var.lab_role
+    put_item {
+      table_name = aws_dynamodb_table.sensor_west.name
+    }
+  }
+}
+
+resource "aws_iot_topic_rule" "sensor_center_topic_rule" {
+  name        = "SensorCenterRule"
+  description = "Sensor Center rule to send data to DynamoDB"
+  enabled     = true
+
+  sql         = "SELECT * FROM 'sensor_center'"
+  sql_version = "2016-03-23"
+
+  dynamodbv2 {
+    role_arn = var.lab_role
+    put_item {
+      table_name = aws_dynamodb_table.sensor_center.name
+    }
   }
 }
