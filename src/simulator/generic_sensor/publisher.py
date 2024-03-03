1ea730f1649257c803c743a5ec014f9a86b17da9
Elisa Flemer <elisaflemer@gmail.com>
2024-03-03 16:03:44 -0300

    all tests

--- src/simulator/generic_sensor/publisher.py
+++ src/simulator/generic_sensor/publisher.py
@@ -1,4 +1,17 @@
-from awscrt import mqtt, http
+"""
+Overview:
+This script connects to AWS IoT Core, reads sensor configuration from a JSON file,
+reads sensor data from a CSV file, and publishes the data to an MQTT topic.
+
+Dependencies:
+- Python packages: awscrt, awsiot, sys, threading, time, json, uuid, argparse
+
+Usage:
+Run the script with command-line arguments '--sensor' and '--region'.
+
+"""
+
+from awscrt import mqtt
 from awsiot import mqtt_connection_builder
 import sys
 import threading
@@ -8,8 +21,7 @@ import uuid
 import argparse
 
 class Configuration:
-    def __init__(self, unit, transmission_rate_hz,
-                 region, sensor_type, qos):
+    def __init__(self, unit, transmission_rate_hz, region, sensor_type, qos):
         self.unit = unit
         self.transmission_rate_hz = transmission_rate_hz
         self.region = region
@@ -17,8 +29,7 @@ class Configuration:
         self.qos = qos
 
 class Data:
-    def __init__(self, value, unit, transmission_rate_hz, region,
-                 sensor_type, timestamp, qos):
+    def __init__(self, value, unit, transmission_rate_hz, region, sensor_type, qos):
         self.value = value
         self.unit = unit
         self.transmission_rate_hz = transmission_rate_hz
@@ -26,8 +37,7 @@ class Data:
         self.sensor_type = sensor_type
         self.timestamp = str(time.time())
         self.qos = qos
-        self.sensor_id: uuid.uuid4()
-
+        self.sensor_id = uuid.uuid4()
 
 def read_config(filename):
     with open(filename, 'r') as file:
@@ -35,18 +45,15 @@ def read_config(filename):
         config = Configuration(**config_json)
     return config
 
-
 def read_csv(path):
     with open(path, 'r') as csv:
         data = [float(line.strip()) for line in csv]
     return data
 
-
 def create_json_message(config, rounded_value):
     timestamp = time.time()
     data = Data(rounded_value, config.unit, config.transmission_rate_hz,
-                config.region, config.sensor_type,
-                timestamp, config.qos)
+                config.region, config.sensor_type, timestamp, config.qos)
     return json.dumps(data.__dict__)
 
 def get_args():
@@ -57,7 +64,6 @@ def get_args():
     return args
 
 def connect_mqtt(endpoint, port, cert_path, key_path, ca_cert, client_id):
-    
     mqtt_connection = mqtt_connection_builder.mtls_from_path(
         endpoint=endpoint,
         port=port,
@@ -74,52 +80,38 @@ def connect_mqtt(endpoint, port, cert_path, key_path, ca_cert, client_id):
         on_connection_closed=on_connection_closed)
     
     print(f"Connecting to {endpoint} with client ID '{client_id}'...")
-
     connect_future = mqtt_connection.connect()
     connect_future.result()
     return mqtt_connection, connect_future.result()
-
 
 def on_connection_interrupted(connection, error, **kwargs):
     print("Connection interrupted. error: {}".format(error))
 
 def on_connection_resumed(connection, return_code, session_present, **kwargs):
     print("Connection resumed. return_code: {} session_present: {}".format(return_code, session_present))
-
     if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
         print("Session did not persist. Resubscribing to existing topics...")
         resubscribe_future, _ = connection.resubscribe_existing_topics()
-
-        # Cannot synchronously wait for resubscribe result because we're on the connection's event-loop thread,
-        # evaluate result with a callback instead.
         resubscribe_future.add_done_callback(on_resubscribe_complete)
-
 
 def on_resubscribe_complete(resubscribe_future):
     resubscribe_results = resubscribe_future.result()
     print("Resubscribe results: {}".format(resubscribe_results))
-
     for topic, qos in resubscribe_results['topics']:
         if qos is None:
             sys.exit("Server rejected resubscribe to topic: {}".format(topic))
 
-
-# Callback when the subscribed topic receives a message
 def on_message_received(topic, payload, dup, qos, retain, **kwargs):
     print("Received message from topic '{}': {}".format(topic, payload))
 
-# Callback when the connection successfully connects
 def on_connection_success(connection, callback_data):
     assert isinstance(callback_data, mqtt.OnConnectionSuccessData)
     print("Connection Successful with return code: {} session present: {}".format(callback_data.return_code, callback_data.session_present))
 
-# Callback when a connection attempt fails
 def on_connection_failure(connection, callback_data):
     assert isinstance(callback_data, mqtt.OnConnectionFailureData)
     print("Connection failed with error code: {}".format(callback_data.error))
 
-
-# Callback when a connection has been disconnected or shutdown successfully
 def on_connection_closed(connection, callback_data):
     print("Connection closed")
 
@@ -149,7 +141,7 @@ if __name__ == '__main__':
     data = read_csv(csv_path)
     print(f'Topic: sensor/{config.region}/{config.sensor_type}')
 
-    interval = 1/config.transmission_rate_hz
+    interval = 1 / config.transmission_rate_hz
     for value in data:
         rounded_value = round(value, 2)
         
--- src/tests/test_simulator.py
+++ src/tests/test_simulator.py
@@ -1,49 +1,84 @@
-from simulator.generic_sensor import publisher
-from awscrt import mqtt, http
+"""
+Overview:
+This script defines and executes various tests for the 'publisher' module, which interacts with
+AWS IoT Core using MQTT to publish and subscribe to topics. The tests cover connection, publishing,
+authentication, topic authorization, and data insertion into DynamoDB.
+
+Dependencies:
+- Python packages: time, json, pytest, boto3, uuid
+- External modules: simulator.generic_sensor.publisher, awscrt.mqtt, awscrt.http
+
+Constants:
+- IOT_ENDPOINT: AWS IoT Core endpoint
+- PORT: MQTT port
+- CERT_PATH, KEY_PATH, CA_CERT: Paths to certificate and key files
+- CLIENT_ID_PUB, CLIENT_ID_SUB, CLIENT_ID_PUBSUB, CLIENT_ID_DYNAMO: MQTT client IDs
+- TEST_TOPIC, AUTH_TOPIC, WRONG_TOPIC: MQTT topics for testing
+
+Global Variables:
+- received_message: Dictionary to store received MQTT messages during tests
+
+Functions:
+- test_connect: Test connection to AWS IoT Core
+- test_publish: Test publishing a message and subscribing to the same topic
+- test_authenticate: Test authentication with various certificate configurations
+- test_publishing_topic_authorization: Test topic authorization for publishing
+- test_subscribing_topic_authorization: Test topic authorization for subscribing
+- test_insertion_into_dynamodb: Test insertion of data into DynamoDB
+
+Callbacks:
+- on_message_received: Callback when a message is received on a subscribed topic
+
+Usage:
+Run 'pytest -v' on the src directory to execute the tests.
+"""
+
 import time
 import json
 import pytest
 import boto3
 import uuid
+from simulator.generic_sensor import publisher
+from awscrt import mqtt, http
+
+# Constants
+IOT_ENDPOINT = 'a32jmg845uczmw-ats.iot.us-east-1.amazonaws.com'
+PORT = 8883
+CERT_PATH = './authentication-keys/test_cert.pem'
+KEY_PATH = './authentication-keys/test_key.pem'
+CA_CERT = './authentication-keys/root-CA.crt'
+CLIENT_ID_PUB = 'test_pub'
+CLIENT_ID_SUB = 'test_sub'
+CLIENT_ID_PUBSUB = 'test_pubsub'
+CLIENT_ID_DYNAMO = 'test_sub'
+TEST_TOPIC = 'test/test'
+AUTH_TOPIC = 'test/publishing_topic_authorization'
+WRONG_TOPIC = 'wrong_topic'
 
 
-
-global received_message
+# Global variables
 received_message = {'message': 'no message received'}
 
+
 def test_connect():
-    endpoint = 'a32jmg845uczmw-ats.iot.us-east-1.amazonaws.com'
-    port = 8883
-    cert_path = './authentication-keys/test_cert.pem'
-    key_path = './authentication-keys/test_key.pem'
-    ca_cert = './authentication-keys/root-CA.crt'
-    client_id = 'test_pub'
-    connection, result = publisher.connect_mqtt(endpoint, port, cert_path, key_path, ca_cert, client_id)
-    
+    connection, result = publisher.connect_mqtt(IOT_ENDPOINT, PORT, CERT_PATH, KEY_PATH, CA_CERT, CLIENT_ID_PUB)
     assert result['session_present']
+
 
 def test_publish():
     global received_message
     received_message = {'message': 'no message received'}
-    endpoint = 'a32jmg845uczmw-ats.iot.us-east-1.amazonaws.com'
-    port = 8883
 
-    cert_path = './authentication-keys/test_cert.pem'
-    key_path = './authentication-keys/test_key.pem'
-    ca_cert = './authentication-keys/root-CA.crt'
-    client_id = 'test_sub'
-    mqtt_connection, _ = publisher.connect_mqtt(endpoint, port, cert_path, key_path, ca_cert, client_id)
-    topic = 'test/test'
-    id = str(uuid.uuid4())
-    message = {'test': 'this is the test message',
-                'id': str(uuid.uuid4())}
+    mqtt_connection, _ = publisher.connect_mqtt(IOT_ENDPOINT, PORT, CERT_PATH, KEY_PATH, CA_CERT, CLIENT_ID_SUB)
+    message_id = str(uuid.uuid4())
+    message = {'test': 'this is the test message', 'id': message_id}
 
-    publisher.publish_message(mqtt_connection, topic, json.dumps(message))
+    publisher.publish_message(mqtt_connection, TEST_TOPIC, json.dumps(message))
 
     subscribe_future, _ = mqtt_connection.subscribe(
-    topic=topic,
-    qos=mqtt.QoS.AT_LEAST_ONCE,
-    callback=on_message_received
+        topic=TEST_TOPIC,
+        qos=mqtt.QoS.AT_LEAST_ONCE,
+        callback=on_message_received
     )
 
     subscribe_result = subscribe_future.result()
@@ -51,131 +86,86 @@ def test_publish():
     time.sleep(1)
     assert received_message == message
 
-def test_authenticate():
-    endpoint = 'a32jmg845uczmw-ats.iot.us-east-1.amazonaws.com'
-    port = 8883
-    cert_path = './authentication-keys/test_cert.pem'
-    key_path = './authentication-keys/test_key.pem'
-    ca_cert = './authentication-keys/root-CA.crt'
-    empty_cert_path = './authentication-keys/empty_cert.pem'
-    empty_key_path = './authentication-keys/empty_key.pem'
-    empty_ca_cert = './authentication-keys/empty_root-CA.crt'
 
-    client_id = 'test_pub'
-    _, result = publisher.connect_mqtt(endpoint, port, cert_path, key_path, ca_cert, client_id)
+def test_authenticate():
+    _, result = publisher.connect_mqtt(IOT_ENDPOINT, PORT, CERT_PATH, KEY_PATH, CA_CERT, CLIENT_ID_PUB)
     assert result['session_present']
 
     with pytest.raises(Exception):
-        _, result = publisher.connect_mqtt(endpoint, port, empty_cert_path, key_path, ca_cert, client_id)
+        _, result = publisher.connect_mqtt(IOT_ENDPOINT, PORT, './authentication-keys/empty_cert.pem', KEY_PATH, CA_CERT, CLIENT_ID_PUB)
         assert not result['session_present']
 
     with pytest.raises(Exception):
-        _, result = publisher.connect_mqtt(endpoint, port, cert_path, empty_key_path, ca_cert, client_id)
+        _, result = publisher.connect_mqtt(IOT_ENDPOINT, PORT, CERT_PATH, './authentication-keys/empty_key.pem', CA_CERT, CLIENT_ID_PUB)
         assert not result['session_present']
 
     with pytest.raises(Exception):
-        _, result = publisher.connect_mqtt(endpoint, port, cert_path, key_path, empty_ca_cert, client_id)
+        _, result = publisher.connect_mqtt(IOT_ENDPOINT, PORT, CERT_PATH, KEY_PATH, './authentication-keys/empty_root-CA.crt', CLIENT_ID_PUB)
         assert not result['session_present']
 
 
 def test_publishing_topic_authorization():
     global received_message
     received_message = {'message': 'no message received'}
-    endpoint = 'a32jmg845uczmw-ats.iot.us-east-1.amazonaws.com'
-    port = 8883
 
-    cert_path = './authentication-keys/test_cert.pem'
-    key_path = './authentication-keys/test_key.pem'
-    ca_cert = './authentication-keys/root-CA.crt'
-    client_id = 'test_pubsub'
-    mqtt_connection, _ = publisher.connect_mqtt(endpoint, port, cert_path, key_path, ca_cert, client_id)
-    wrong_topic = 'wrong_topic'
-    correct_topic = 'test/publishing_topic_authorization'
-    message_id = str(uuid.uuid4())
-    message = {'test': 'this is the test message',
-                'id': message_id}
+    mqtt_connection, _ = publisher.connect_mqtt(IOT_ENDPOINT, PORT, CERT_PATH, KEY_PATH, CA_CERT, CLIENT_ID_PUBSUB)
 
     subscribe_future, _ = mqtt_connection.subscribe(
-    topic=correct_topic,
-    qos=mqtt.QoS.AT_LEAST_ONCE,
-    callback=on_message_received
+        topic=AUTH_TOPIC,
+        qos=mqtt.QoS.AT_LEAST_ONCE,
+        callback=on_message_received
     )
 
     subscribe_result = subscribe_future.result()
 
-    publisher.publish_message(mqtt_connection, wrong_topic, json.dumps(message))
+    message_id = str(uuid.uuid4())
+    message = {'test': 'this is the test message', 'id': message_id}
+
+    publisher.publish_message(mqtt_connection, WRONG_TOPIC, json.dumps(message))
 
     time.sleep(1)
-   
+
     assert not received_message == message
-        
+
 
 def test_subscribing_topic_authorization():
     global received_message
     received_message = {'message': 'no message received'}
-    endpoint = 'a32jmg845uczmw-ats.iot.us-east-1.amazonaws.com'
-    port = 8883
 
-    cert_path = './authentication-keys/test_cert.pem'
-    key_path = './authentication-keys/test_key.pem'
-    ca_cert = './authentication-keys/root-CA.crt'
-    client_id = 'test_sub'
-    mqtt_connection, _ = publisher.connect_mqtt(endpoint, port, cert_path, key_path, ca_cert, client_id)
-    correct_topic = 'test/test'
-    wrong_topic = 'wrong_topic'
-    message = {'test': 'this is the test message',
-                'id': str(uuid.uuid4())}
+    mqtt_connection, _ = publisher.connect_mqtt(IOT_ENDPOINT, PORT, CERT_PATH, KEY_PATH, CA_CERT, CLIENT_ID_SUB)
 
     subscribe_future, _ = mqtt_connection.subscribe(
-    topic=correct_topic,
-    qos=mqtt.QoS.AT_LEAST_ONCE,
-    callback=on_message_received
+        topic=TEST_TOPIC,
+        qos=mqtt.QoS.AT_LEAST_ONCE,
+        callback=on_message_received
     )
 
     subscribe_result = subscribe_future.result()
-    assert subscribe_result['topic'] == correct_topic
+    assert subscribe_result['topic'] == TEST_TOPIC
 
     subscribe_future, _ = mqtt_connection.subscribe(
-    topic=wrong_topic,
-    qos=mqtt.QoS.AT_LEAST_ONCE,
-    callback=on_message_received
+        topic=WRONG_TOPIC,
+        qos=mqtt.QoS.AT_LEAST_ONCE,
+        callback=on_message_received
     )
     time.sleep(1)
     assert not subscribe_future.done()
 
+
 def test_insertion_into_dynamodb():
-
-    endpoint = 'a32jmg845uczmw-ats.iot.us-east-1.amazonaws.com'
-    port = 8883
-
-    cert_path = './authentication-keys/test_cert.pem'
-    key_path = './authentication-keys/test_key.pem'
-    ca_cert = './authentication-keys/root-CA.crt'
-    client_id = 'test_sub'
-    mqtt_connection, _ = publisher.connect_mqtt(endpoint, port, cert_path, key_path, ca_cert, client_id)
-    topic = 'test/test'
-    timestamp = time.time()
+    mqtt_connection, _ = publisher.connect_mqtt(IOT_ENDPOINT, PORT, CERT_PATH, KEY_PATH, CA_CERT, CLIENT_ID_DYNAMO)
     message_id = str(uuid.uuid4())
-    message = {'test': 'this is the test message',
-                'id': message_id}
+    message = {'test': 'this is the test message', 'id': message_id}
 
-    publisher.publish_message(mqtt_connection, topic, json.dumps(message))
+    publisher.publish_message(mqtt_connection, TEST_TOPIC, json.dumps(message))
 
     dynamodb = boto3.resource('dynamodb')
-
-    # Specify the table name
     table_name = 'sensorTest'
-
-    # Access the DynamoDB table
     table = dynamodb.Table(table_name)
 
-    response = table.scan(
-    FilterExpression=boto3.dynamodb.conditions.Attr('id').eq(message_id))
+    response = table.scan(FilterExpression=boto3.dynamodb.conditions.Attr('id').eq(message_id))
     assert len(response['Items']) >= 1
 
-    
-
-    
 
 def on_message_received(topic, payload, **kwargs):
     print("Received message from topic '{}': {}".format(topic, payload))
@@ -183,4 +173,3 @@ def on_message_received(topic, payload, **kwargs):
     payload = json.loads(decoded)
     global received_message
     received_message = payload
-
