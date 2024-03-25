ae856cc6801afc4abfb5019aee730a7fb1b31fbf
Gustavo Ferreira <gustavo.oliveira@sou.inteli.edu.br>
2024-03-25 08:28:11 -0300

    fix: update pipeline and remove variables

--- infrastructure/terraform/global/variables.tf
+++ infrastructure/terraform/global/variables.tf
@@ -21,41 +21,17 @@ variable "default_availability_zone" {
 variable "lab_role" {
   type        = string
   description = "Lab role"
-  default     = "arn:aws:iam::058264141216:role/LabRole"
+  default     = "arn:aws:iam::730335212171:role/LabRole"
 }
 
 variable "dynamodb_arn" {
   type        = string
   description = "DynamoDB ARN"
-  default     = "arn:aws:dynamodb:us-east-1::058264141216:table/*"
+  default     = "arn:aws:dynamodb:us-east-1::730335212171:table/*"
 }
 
 variable "bucket_state" {
   type        = string
   description = "S3 bucket for state"
-  default     = "infrastructure-state-terraform20240322115712961400000002"
-}
-
-variable "kafka_bootstrap_servers" {
-  type        = string
-  description = "Kafka bootstrap servers"
-  default     = "pkc-p11xm.us-east-1.aws.confluent.cloud:9092"
-}
-
-variable "kafka_username" {
-  type        = string
-  description = "Kafka username"
-  default     = "R23TSC2632N5DZ6U"
-}
-
-variable "kafka_password" {
-  type        = string
-  description = "Kafka password"
-  default     = "Vq7xv/7Hay9K64Jzq56ak7VsuP5WMpawYUm5GxV69O4YCKTx5s314LCf9Bd3QJP0"
-}
-
-variable "kafka_topic" {
-  type        = string
-  description = "Kafka topic"
-  default     = "regions"
+  default     = "infrastructure-state-terraform20240325111449711200000002"
 }
