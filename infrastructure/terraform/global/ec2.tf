211cc30c062435a9b6955caa211bc5549fabcb8d
Gustavo Ferreira <gustavo.oliveira@sou.inteli.edu.br>
2024-03-27 23:10:15 -0300

    fix: launch file configuration

--- infrastructure/terraform/global/ec2.tf
+++ infrastructure/terraform/global/ec2.tf
@@ -3,41 +3,42 @@ resource "aws_launch_template" "sh_ec2_launch_templ" {
   image_id      = "ami-080e1f13689e07408"
   instance_type = "t3.medium"
   key_name      = "metabase_ec2_key"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
   user_data = base64encode(<<-EOF
 #!/bin/bash
-
 sudo apt-get update -y
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-# Cria e configura o docker-compose.yml
-cat > docker-compose.yml <<-EOF_DOCKER
-version: '3.8'
+sudo touch /var/docker-compose.yml
+
+sudo echo "version: '3.7'
 services:
   metabase:
     image: metabase/metabase:latest
     environment:
       MB_DB_TYPE: postgres
       MB_DB_DBNAME: postgres
-      MB_DB_PORT: "5432"
+      MB_DB_PORT: '5432'
       MB_DB_USER: postgres
       MB_DB_PASS: postgres123
-      MB_DB_HOST: ${aws_db_instance.main_postgresql_db.address}
+      MB_DB_HOST: ${}
     ports:
-      - "80:3000"
-EOF_DOCKER
-
-# Inicia o Metabase usando Docker Compose
-docker compose up -d
+      - '80:3000'" > /var/docker-compose.yml
+(sudo systemctl enable docker)
+(cd /var && sudo docker compose up -d)
 EOF
   )
 
